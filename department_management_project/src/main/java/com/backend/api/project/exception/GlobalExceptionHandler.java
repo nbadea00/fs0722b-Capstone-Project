7ab5b4ffce48fc5f849ae21dc7c13562de6a7fdb
nbadea00 <nbadea61@gmail.com>
2023-03-31 14:31:46 +0200

    project-exception-service

---
+++ department_management_project/src/main/java/com/backend/api/project/exception/GlobalExceptionHandler.java
@@ -0,0 +1,89 @@
+package com.backend.api.project.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import jakarta.persistence.EntityExistsException;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    // handle specific exceptions
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException exception,
+                                                                        WebRequest webRequest){
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+    
+    @ExceptionHandler(EntityExistsException.class)
+    public ResponseEntity<String> manageEntityExistsException(EntityExistsException exception,
+                                                                        WebRequest webRequest){
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.FOUND);
+    }
+
+    @ExceptionHandler(MyAPIException.class)
+    public ResponseEntity<String> handleBlogAPIException(MyAPIException exception,
+                                                                        WebRequest webRequest){
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+    
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Set<String>> handleConstraintViolationException(ConstraintViolationException exception,
+                                                                        WebRequest webRequest){
+    	
+    	Set<ConstraintViolation<?>> constraintViolations = exception.getConstraintViolations();
+
+    	
+    	Set<String> messages = new HashSet<>(constraintViolations.size());
+    	messages.addAll(constraintViolations.stream()
+    	        .map(constraintViolation -> constraintViolation.getMessage())
+    	        .collect(Collectors.toList()));
+    	
+        return new ResponseEntity<>(messages, HttpStatus.BAD_REQUEST);
+    }
+    
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException exception,
+                                                                        WebRequest webRequest){
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+    
+    // global exceptions
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGlobalException(Exception exception,
+                                                               WebRequest webRequest){
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatusCode status,
+                                                                  WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) ->{
+            String fieldName = ((FieldError)error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+}
---
+++ department_management_project/src/main/java/com/backend/api/project/project/ProjectService.java
@@ -0,0 +1,35 @@
+package com.backend.api.project.project;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import com.backend.api.project.exception.ResourceNotFoundException;
+
+@Service
+public class ProjectService {
+
+	@Autowired ProjectRepository repo;
+	
+	public Project save(Project project) {
+		return repo.save(project);
+	}
+	
+	public Project update(Project project) {
+		return repo.save(project);
+	}
+	
+	public Project findBuId(Long id) {
+		return repo.findById(id).orElseThrow(() -> new ResourceNotFoundException("Project", "id",  id));
+	}
+	
+	public Page<Project> findAll(int page, int dim) {
+		return repo.findAll(PageRequest.of(page, dim));
+	}
+	
+	public String delById(Long id) {
+		repo.deleteById(id);
+		return "Project remuved";
+	}
+}
